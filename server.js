7845dd28dac46564b4852f028f108ff619715635
Mario Quintana <pyrros2000@gmail.com>
2018-07-11 16:52:48 -0700

    created simple server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const { createServer } = require('http');
+const app = require('./lib/app');
+
+const PORT = 5000;
+
+const server = createServer(app).listen(PORT, () => {
+    console.log('server running on', server.address().port);
+});
