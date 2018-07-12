2cf212d21ec1497648fa746990ee2b24c673b227
carriecarter <carrie@carrie-carter.com>
2018-07-12 13:02:29 -0700

    starting testing files

---
+++ test/coffee.test.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { assert } = chai;
+chai.use(chaiHttp);
+const app = require('../lib/app');
+const client = require('../lib/db-client');
+
