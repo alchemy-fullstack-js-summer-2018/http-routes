cb5e5d637ef1280e7bf5d10763490cf2544026a3
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-12 11:19:30 -0700

    Move data into test and implement post function

--- db/load-db.js
+++ db/load-db.js
@@ -1,41 +1,23 @@
-/* eslint no-console: off, quotes: off */
-const client = require('../lib/db-client');
+// /* eslint no-console: off, quotes: off */
+// const client = require('../lib/db-client');
 
-const movies = [
-    {
-        name: 'terminator',
-        genre: 'sci-fi',
-        year: 1984
-    },
-    {
-        name: 'collateral',
-        genre: 'action',
-        year: 2004
-    },
-    {
-        name: 'coraline',
-        genre: 'horror',
-        year: 2009
-    }
-];
+// const moviePromises = movies.map(movie => {
+//     return client.query(`
+//         INSERT INTO movies (name, genre, year)
+//         VALUES ($1, $2, $3);
+//     `,
+//     [movie.name, movie.genre, movie.year]
+//     );
+// });
 
-const moviePromises = movies.map(movie => {
-    return client.query(`
-        INSERT INTO movies (name, genre, year)
-        VALUES ($1, $2, $3);
-    `,
-    [movie.name, movie.genre, movie.year]
-    );
-});
-
-Promise.all(moviePromises)
-    .then(() => {
-        return client.query(`
-            SELECT * FROM movies;
-        `);
-    })
-    .then(
-        () => console.log('load successful'),
-        err => console.error(err)
-    )
-    .then(() => client.end());
+// Promise.all(moviePromises)
+//     .then(() => {
+//         return client.query(`
+//             SELECT * FROM movies;
+//         `);
+//     })
+//     .then(
+//         () => console.log('load successful'),
+//         err => console.error(err)
+//     )
+//     .then(() => client.end());
--- test/movies.test.js
+++ test/movies.test.js
@@ -3,35 +3,55 @@ const chaiHttp = require('chai-http');
 const { assert } = chai;
 chai.use(chaiHttp);
 const app = require('../lib/app');
-// const client = require('../lib/db-client');
-
-const movies = [
-    {
-        name: 'terminator',
-        genre: 'sci-fi',
-        id: 1,
-        year: 1984
-    },
-    {
-        name: 'collateral',
-        genre: 'action',
-        id: 2,
-        year: 2004
-    },
-    {
-        name: 'coraline',
-        genre: 'horror',
-        id: 3,
-        year: 2009
-    }
-];
+const client = require('../lib/db-client');
 
 describe('movies API', () => {
+    beforeEach(() => client.query('DELETE FROM movies;'));
+
+    let terminator = {
+        name: 'terminator',
+        genre: 'sci-fi',
+        year: 1984
+    };
+    
+    let collateral = {
+        name: 'collateral',
+        genre: 'action',
+        year: 2004
+    };
+    
+    let coraline = {
+        name: 'coraline',
+        genre: 'horror',
+        year: 2009
+    };
+
+    function save(movie) {
+        return chai.request(app)
+            .post('/movies')
+            .send(movie)
+            .then(({ body }) => {
+                movie.id = body.id;
+                assert.deepEqual(body, movie);
+            });
+    }
+
+    beforeEach(() => {
+        return save(terminator);
+    });
+    
+    beforeEach(() => {
+        return save(collateral);
+    });
+    beforeEach(() => {
+        return save(coraline);
+    });
+
     it('GET movie by id', () => {
         return chai.request(app)
-            .get('/movies/1')
+            .get(`/movies/${terminator.id}`)
             .then(({ body }) => {
-                assert.deepEqual(body.name, 'terminator');
+                assert.deepEqual(body, terminator);
             });
     });
 
@@ -39,7 +59,19 @@ describe('movies API', () => {
         return chai.request(app)
             .get('/movies')
             .then(({ body }) => {
-                assert.deepEqual(body, movies);
+                assert.deepEqual(body, [terminator, collateral, coraline]);
+            });
+    });
+
+    it('DELETE movie', () => {
+        return chai.request(app)
+            .del('/movies/1')
+            .then(() => {
+                return chai.request(app)
+                    .get('/movies/1');
+            })
+            .then(res => {
+                assert.equal(res.status, 404);
             });
     });
 });
