13c00413be2639e0b2762c673c7115d6d4c266b5
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-11 20:54:11 -0700

    404 test passing

--- test/app.test.js
+++ test/app.test.js
@@ -1,16 +1,22 @@
+const { join } = require('path');
 const app = require('../lib/app');
 require('dotenv').config({ path: join(__dirname, '.env.test') });
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { assert } = chai;
 chai.use(chaiHttp);
-// const client = require('../lib/db-client');
+const client = require('../lib/db-client');
 
 describe('app level 404', () => {
-    return chai.request(app)
-        .get ('/not-found')
-        .then(res => {
-            assert.equal(res.status, 404);
-            assert.equal(res.type, 'application/json');
-        });
+
+    it('returns 404 on not found', () => {
+        return chai.request(app)
+            .get ('/not-found')
+            .then(res => {
+                assert.equal(res.status, 404);
+                assert.equal(res.type, 'application/json');
+            });
+    });
 });
+
+after(() => client.end());
