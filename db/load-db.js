d1a31774edf315dc21fb161e6e8277146e4b4a17
Easton Gorishek <ieaston26@gmail.com>
2018-07-12 12:31:44 -0700

    save method done

--- db/load-db.js
+++ db/load-db.js
@@ -1,51 +1,51 @@
-const client = require('../lib/db-client');
+// const client = require('../lib/db-client');
 
-const archer = [
-    {
-        'name': 'Sterling Archer',
-        'sex': 'male', 
-        'quote': 'Phrasing'
-    },
+// const archer = [
+//     {
+//         'name': 'Sterling Archer',
+//         'sex': 'male', 
+//         'quote': 'Phrasing'
+//     },
 
-    {
-        'name': 'Lana Kane',
-        'sex': 'female',
-        'quote': 'Yup.'
-    }
-];
+//     {
+//         'name': 'Lana Kane',
+//         'sex': 'female',
+//         'quote': 'Yup.'
+//     }
+// ];
 
-const archerTable = archer.map(character => {
-    return client.query(`
-        INSERT INTO archer (
-            name,
-            sex,
-            quote
-        )
-        VALUES ($1, $2, $3);
-        `,
-    [
-        character.name,
-        character.sex,
-        character.quote
-    ]
-    );
-});
+// const archerTable = archer.map(character => {
+//     return client.query(`
+//         INSERT INTO archer (
+//             name,
+//             sex,
+//             quote
+//         )
+//         VALUES ($1, $2, $3);
+//         `,
+//     [
+//         character.name,
+//         character.sex,
+//         character.quote
+//     ]
+//     );
+// });
 
-Promise.all(archerTable)
-    .then(() => {
-        return client.query(`
-        SELECT *
-        FROM archer;
-        `
-        );
-    })
-    .then(result => {
-        console.log(result.rows);
-    })
-    .then(
-        () => console.log('load successful'),
-        err => console.error(err)
-    )
-    .then(() => {
-        client.end();
-    });
+// Promise.all(archerTable)
+//     .then(() => {
+//         return client.query(`
+//         SELECT *
+//         FROM archer;
+//         `
+//         );
+//     })
+//     .then(result => {
+//         console.log(result.rows);
+//     })
+//     .then(
+//         () => console.log('load successful'),
+//         err => console.error(err)
+//     )
+//     .then(() => {
+//         client.end();
+//     });
--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,39 @@ const app = require('../lib/app');
 const client = require('../lib/db-client');
 
 
-describe('HTTP methods and paths', () => {
+describe('Archer API', () => {
+
+    beforeEach(() => client.query('DELETE FROM archer'));
+
+    let archer = {
+        name: 'Sterling Archer',
+        sex: 'male',
+        quote: 'Phrasing.'
+    };
+
+    let lana = {
+        name: 'Lana Kane',
+        sex: 'male',
+        quote: 'Yup.'
+    };
+
+    function save(character) {
+        return chai.request(app)
+            .post('/archer')
+            .send(character)
+            .then(({ body }) => {
+                character.id = body.id;
+                assert.deepEqual(body, character);
+            });
+    }
+
+    beforeEach(() => {
+        return save(archer);
+    });
+
+    it('saves a character', () => {
+        assert.ok(archer.id);
+    });
 
     it('returns 404 on not found', () => {
         return chai.request(app)
