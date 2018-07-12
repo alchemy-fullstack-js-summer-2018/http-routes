1fc4b2266ca9f2834ae061d84fce07bbee5a3a9f
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-12 12:14:40 -0700

    Save location working

---
+++ test/locations.test.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { assert } = chai;
+chai.use(chaiHttp);
+const app = require('../lib/app');
+const client = require('../lib/db-client');
+
+describe('Locations API', () => {
+
+    beforeEach(() => client.query('DELETE FROM locations'));
+
+    let tokyo = {
+        country: 'Japan',
+        city: 'Tokyo',
+        food: 'Sushi'
+    };
+
+    let miami = {
+        country: 'USA',
+        city: 'Miami',
+        food: 'Seafood'
+    };
+
+    function save(location) {
+        return chai.request(app)
+            .post('/locations')
+            .send(location)
+            .then(({ body }) => {
+                location.id = body.id;
+                assert.deepEqual(body, location);
+            });
+    }
+
+    beforeEach(() => {
+        return save(tokyo);
+    });
+
+    beforeEach(() => {
+        return save(miami);
+    });
+
+    it('Saves a location', () => {
+        assert.ok(tokyo.id);
+    });
+
+});
