478fe6c9aa9f5efe55f2e2ffce3a08b373c6b7c3
rbtprograms <rbt61690@gmail.com>
2018-07-11 17:30:33 -0700

    create db done

--- db/create-db.js
+++ db/create-db.js
@@ -1,1 +1,20 @@
 const client = require('../lib/db-client');
+
+client.query(`
+    CREATE TABLE IF NOT EXISTS positions(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(256) UNIQUE NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS players(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(256),
+        position_id INTEGER NOT NULL REFERENCES positions (id),
+        description VARCHAR(256)
+    );
+`)
+    .then(
+        () => console.log('db task successful'),
+        err => console.error(err)
+    )
+    .then(() => client.end());
