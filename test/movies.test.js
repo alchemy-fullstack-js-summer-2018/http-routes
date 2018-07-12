51aef891c2f88a78fe822b24748ba453c4f3fa08
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-12 10:48:41 -0700

    Add function for getting all movies and test

--- test/movies.test.js
+++ test/movies.test.js
@@ -5,12 +5,41 @@ chai.use(chaiHttp);
 const app = require('../lib/app');
 // const client = require('../lib/db-client');
 
+const movies = [
+    {
+        name: 'terminator',
+        genre: 'sci-fi',
+        id: 1,
+        year: 1984
+    },
+    {
+        name: 'collateral',
+        genre: 'action',
+        id: 2,
+        year: 2004
+    },
+    {
+        name: 'coraline',
+        genre: 'horror',
+        id: 3,
+        year: 2009
+    }
+];
+
 describe('movies API', () => {
     it('GET movie by id', () => {
         return chai.request(app)
             .get('/movies/1')
             .then(({ body }) => {
                 assert.deepEqual(body.name, 'terminator');
+            });
+    });
+
+    it('GET movies', () => {
+        return chai.request(app)
+            .get('/movies')
+            .then(({ body }) => {
+                assert.deepEqual(body, movies);
             });
     });
 });
