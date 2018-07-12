d90a643b2cc527f5bebdec6a4d4b381ce6fdcaab
Mario Quintana <pyrros2000@gmail.com>
2018-07-12 10:15:58 -0700

    created model.js for cars to use in routes

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,11 @@
-// const { join } = require('path');
-// require('dotenv').config({ path: join(__dirname, 'env.test') });
+const { join } = require('path');
+require('dotenv').config({ path: join(__dirname, 'env.test') });
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { assert } = chai;
 chai.use(chaiHttp);
 const app = require('../lib/app');
-// const client = require('../lib/db-client');
+const client = require('../lib/db-client');
 
 describe('app level', () => {
 
@@ -20,4 +20,4 @@ describe('app level', () => {
 
 });
 
-// after(() => client.end());
+after(() => client.end());
