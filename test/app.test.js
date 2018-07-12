ef2153636985667ebc97ba7b964efdacac2de8c3
carriecarter <carrie@carrie-carter.com>
2018-07-12 13:10:52 -0700

    welp. testing confusion

---
+++ test/app.test.js
@@ -0,0 +1,20 @@
+require('dotenv').config({ path: join(__dirname, '.env.test') });
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { assert } = chai;
+chai.use(chaiHttp);
+const app = require('../lib/app');
+const client = require('../lib/db-client');
+
+describe('app level', () => {
+    it('returns 404 not found', () => {
+        return chai.request(app)
+            .get('/not-found')  
+            .then(res => {
+                assert.equal(res.status, 404);
+                assert.equal(res.type, 'application/json');
+            });
+    });
+});
+
+after(() => client.end());
