b4a7fc1063e8d5fae8398c74d68015694e4977aa
Antreo Pukay <acrosswalker@yahoo.com>
2018-07-12 10:29:23 -0700

    psql table created, 404 test passing

---
+++ db/create-db.js
@@ -0,0 +1,17 @@
+/* eslint no-console: off, quotes: off */
+
+const client = require('../lib/db-client');
+
+client.query(`
+    CREATE TABLE IF NOT EXISTS guitars(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(256) UNIQUE NOT NULL
+    );
+`)
+    .then(
+        () => console.log('db task successful'),
+        err => console.error(err)
+
+    )
+    .then(() => client.end());
+    
