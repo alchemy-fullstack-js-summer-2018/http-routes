5c3b3ecabd8719df9977bdfd65f8304ed317b9b6
Rehmsy <sarahrehmer@gmail.com>
2018-07-12 10:29:04 -0700

    workin out load-db

--- db/load-db.js
+++ db/load-db.js
@@ -1,1 +1,41 @@
+
+/* eslint no-console: off, quotes: off */
+
 const client = require('../lib/db-client');
+
+//const shows = ['alone', 'bachelorette'];
+const shows = [{
+    name: 'Alone',
+    description: 'Weirdos surviving for months alone in nature with an ax and a tarp.'
+},
+{
+    name: 'Bachelorette',
+    description: '22 man-babies, 6 bores, and 2 semi-decent humans compete to marry a woman they barely know.'
+}];
+
+const showPromises = shows.map(show => {
+    return client.query(
+        `INSERT INTO shows(name) 
+        VALUES($1, $2)
+        ON CONFLICT DO NOTHING;`,
+        [show.name, show.description]
+    );
+});
+
+Promise.all(showPromises)
+    .then(() => {
+        return client.query(
+            `SELECT * FROM shows`
+        );
+    })
+    .then(result => {
+        console.log(result.rows);
+    })
+    .then(
+        () => console.log('load workin baby'),
+        err => console.error(err)
+    )
+    .then(() => {
+        client.end();
+    });
+    
